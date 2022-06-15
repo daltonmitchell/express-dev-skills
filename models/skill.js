96a71742c401105ae10a3264ec40b65656244b0f
daltonmitchell <dzack34@gmail.com>
2022-06-14 18:23:10 -0700

    adds getOne and show functions

--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,19 @@
 const skills = [
-    {language: 'Javascript', learned: true},
-    {language: 'HTML', learned: true},
-    {language: 'Python', learned: false},
+    {id: 101101, language: 'Javascript', learned: true},
+    {id: 101102, language: 'HTML', learned: true},
+    {id: 101103, language: 'Python', learned: false},
 ];
 
 module.exports = {
     getAll,
+    getOne,
 };
 
 function getAll() {
     return skills;
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id);
 }
--- routes/skills.js
+++ routes/skills.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const skillsCtrl = require('../controllers/skills');
 
 router.get('/', skillsCtrl.index);
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
